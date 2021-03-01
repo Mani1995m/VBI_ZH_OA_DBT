17201edafd9e438d7d168a5c2dc63cbcc20251ed
Subramanian Palaniappan <Subramanianp@VISUALBI.LOCAL>
2021-03-01 10:57:20 +0530

    Designation_DesginationChanges

--- models/stg_zoho/TB_DESIGNATION_FORM.sql
+++ models/stg_zoho/TB_DESIGNATION_FORM.sql
@@ -12,22 +12,17 @@ select
     SRC:"AddedTime"::varchar(15) as added_time_int,
     TO_TIMESTAMP((SRC:"AddedTime")::String, 'DD-mon-YYYY HH:MI:SS') as added_time,
     SRC:"ApprovalStatus"::varchar(25) as approval_status,
-    SRC:"ApprovalTime"::varchar(15) as approval_time_int,
-    TO_TIMESTAMP((SRC:"ApprovalTime")::String, 'DD-mon-YYYY HH12:MI AM') as approval_time,
-    split_part(SRC:"Associate_ID", ' ', -1)::varchar(20) as employee,
-    SRC:"Associate_ID.ID"::varchar(20) as employee_id,
     SRC:"CreatedTime"::varchar(15) as created_time_int,
     TO_TIMESTAMP((SRC:"CreatedTime")::String) as created_time,
-    SRC:"Emergency_Contact_Address"::varchar(200) as emergency_contact_address,
-    SRC:"Emergency_contact_Person"::varchar(50) as emergency_contact_person,
-    SRC:"Emergency_contact_number"::varchar(25) as emergency_contact_number,
+    SRC:"Designation"::varchar(200) as designation,
+    SRC:"MailAlias"::varchar(200) as mail_alias,
     split_part(SRC:"ModifiedBy",'-', 1)::varchar(20) as modified_by,
     SRC:"ModifiedBy.ID"::varchar(25) as modifiedby_id,
     SRC:"ModifiedTime"::varchar(15) as modified_time_int,    
     TO_TIMESTAMP((SRC:"ModifiedTime")::String) as modified_time,
     SRC:"Zoho_ID"::varchar(25) as zoho_id
 
-from {{ source('ZOHO_PEOPLE_FORM', 'TB_HIST_INTERCOMPANY_TRANSFER')}} 
+from {{ source('ZOHO_PEOPLE_FORM', 'TB_HIST_DESIGNATION')}} 
 
 {% if is_incremental() %}
     where modified_time > ( select max(modified_time) from {{ this }} )
