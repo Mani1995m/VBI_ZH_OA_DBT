0272b4a419c87b2f5ee47bce81fd6fdaefa4f281
Subramanian Palaniappan <Subramanianp@VISUALBI.LOCAL>
2021-03-17 13:36:54 +0530

    Switch to ADF source for all zoho forms

--- models/raw_zoho/TB_HIST_BLOB_FORM_PY.sql
+++ models/raw_zoho/TB_HIST_BLOB_FORM_PY.sql
@@ -1,27 +1,22 @@
-{% set form_list = ['employee_adf',
-                    'Project_Department_Changes_adf',
-                    'designation_adf',
-                    'Designation_Change_Request_adf',
-                    'Intercompany_Transfer_adf',                    
-                    'Reporting_Changes_adf'
+{% set form_list = ['employee',
+                    'Project_Department_Changes',
+                    'designation',
+                    'Designation_Change_Request',
+                    'Intercompany_Transfer',                    
+                    'Reporting_Changes'
 ] %}
 
 {% set delta_query %}
     use schema RAW_ZOHO;
     alter session set ERROR_ON_NONDETERMINISTIC_MERGE = false;
     {% for form in form_list %}
-        merge into "ZOHO_OA_EDW"."RAW_ZOHO"."TB_HIST_{{form|upper}}" as target using
+        merge into "ZOHO_OA_EDW"."RAW_ZOHO"."TB_HIST_{{form|upper}}_PY" as target using
             (  select 
                 convert_timezone('UTC', current_timestamp(2))::timestamp_ntz as record_captured_at,
-                l4.key as record_id, 
-                l4.value[0] as SRC1 
+                key as record_id, 
+                value[0] as SRC1 
                from
-                    @STAGE_ZH_HIST_BLOB/vbidw/zoho/{{form}}/{{form}}.json (file_format => FF_JSON_ZOHO) as S, 
-                    lateral flatten( input => $1 ) as l1,
-                    lateral flatten(input => l1.value )l2,
-                    lateral flatten(input => l2.value )l3,
-                    lateral flatten(input => l3.value )l4
-                    
+                    @STAGE_ZH_HIST_BLOB/{{form}}.json (file_format => FF_JSON_ZOHO) as S, lateral flatten( input => $1 )
             ) as sources on
             sources.record_id = target.record_id 
         when matched then 
