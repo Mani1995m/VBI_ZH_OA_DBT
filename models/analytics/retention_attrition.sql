3664be108aec1c18cb7edbd7f4f905c4c58f5775
Subramanian Palaniappan <Subramanianp@VISUALBI.LOCAL>
2021-04-07 17:33:53 +0530

    Month Begin and End Count

--- models/analytics/retention_attrition.sql
+++ models/analytics/retention_attrition.sql
@@ -55,4 +55,40 @@ retention_attrition as(
     order by yr_mon_int
 )
 
+month_start_count as
+(
+
+select  ( year(MONTH_START_DATE) || ' '||  monthname(MONTH_START_DATE)) as year_month,
+        (year(MONTH_START_DATE)*100) + month(MONTH_START_DATE) as yr_mon_int,
+count(employee_id) Active_Emp_Month_Begin
+from (select distinct MONTH_START_DATE from "ZOHO_OA_EDW"."ANALYTICS"."DATE_DIM") X
+left join "ZOHO_OA_EDW"."ANALYTICS"."TB_EMPLOYEE"
+on DATE_OF_JOINING<MONTH_START_DATE
+and (DATE_OF_EXIT>MONTH_START_DATE OR (DATE_OF_EXIT='0001-01-01'))
+where DATE_OF_JOINING!='0001-01-01'
+group by MONTH_START_DATE
+)
+
+month_end_count as(
+
+select left(MONTH_END_DATE,7)Month,count(employee_id) Active_Emp_Month_End  
+from (select distinct MONTH_END_DATE from "ZOHO_OA_EDW"."ANALYTICS"."DATE_DIM") X
+left join "ZOHO_OA_EDW"."ANALYTICS"."TB_EMPLOYEE"
+on DATE_OF_JOINING<MONTH_END_DATE
+and (DATE_OF_EXIT>MONTH_END_DATE OR (DATE_OF_EXIT='0001-01-01'))
+where DATE_OF_JOINING!='0001-01-01'
+group by MONTH_END_DATE
+)
+
+month_start_end as(
+select A.year_month,A.Active_Emp_Month_Begin,B.Active_Emp_Month_End from
+month_start_count A
+left join month_end_count B
+on A.year_month=B.year_month
+)
+
+final as 
+(
+
+)
 select * from retention_attrition
