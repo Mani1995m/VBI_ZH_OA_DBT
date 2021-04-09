5ee62ad952281819496670958615f6f09c3a16bd
Mani1995m <65023140+Mani1995m@users.noreply.github.com>
2021-04-09 17:20:48 +0530

    created deparment model

---
+++ models/stg_oa_api/department.sql
@@ -0,0 +1,19 @@
+{{ 
+    config(materialized='table')
+}}
+
+
+
+with DEPARTMENT AS (
+    select 
+ID :: INT AS ID,
+NAME :: NVARCHAR(100) AS NAME,
+NOTES :: NVARCHAR(250) AS NOTES,
+DELETED :: INT AS DELETED,
+CREATED :: TIMESTAMP AS CREATED,
+UPDATED :: TIMESTAMP AS UPDATED
+
+from  {{ source('OPEN_AIR_FORM', 'TB_HIST_DEPARTMENT')}} 
+"ZOHO_OA_EDW"."RAW_OPENAIR"."DEPARTMENT"
+)
+select * from DEPARTMENT
